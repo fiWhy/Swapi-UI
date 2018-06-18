c02e2859a17771920a4908c468028146ba9438c7
Denis <fiwhy@mail.ru>
2018-06-19 00:56:06 +0300

    Added fonts and logic for UI

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
 var cards = document.getElementsByClassName('card');
 var cardsArray = Array.prototype.slice.call(cards);
-var flipCards = function (cards, time) {
-    var timeModifier = 1;
-
-    var flip = function (card) {
-        card.classList.add('flip');
-    };
-
-    var unflip = function (card) {
-        card.classList.remove('flip');
-    }
-
-
-    cards.forEach(function (card) {
-        setTimeout(function () {
-            flip(card);
-        }, timeModifier++ * time);
-        if (timeModifier == cards.length + 1) {
-            cards.forEach(unflip);
-        }
-    });
-}
-
-var hoverCard = function (card, forTime) {
-    card.classList.add('hover');
-    setTimeout(function () {
-        card.classList.remove('hover');
-    }, forTime)
-}
-
 
 cardsArray.forEach(function (card) {
     card.addEventListener('click', function () {
-        flipCards(_.shuffle(cardsArray), 100);
+        UI.toggleClassName(card, 'active');
+        RequestAPI.get('https://swapi.co/api/people/1/', function (data) {
+            console.log(data);
+        })
+        UI.flipCard(card, 100);
     })
 })
+
+
