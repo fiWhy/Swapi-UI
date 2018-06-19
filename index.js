fe06c7c9c56fb6ef47026811f2d3eee0d4aaf04c
Denis <fiwhy@mail.ru>
2018-06-19 13:07:56 +0300

    Improve JS with BE requests

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-var cards = document.getElementsByClassName('card');
-var cardsArray = Array.prototype.slice.call(cards);
+var wrapper = document.getElementById('wrapper');
+var api = 'https://swapi.co/api/';
 
-cardsArray.forEach(function (card) {
-    card.addEventListener('click', function () {
-        UI.toggleClassName(card, 'active');
-        RequestAPI.get('https://swapi.co/api/people/1/', function (data) {
-            console.log(data);
-        })
-        UI.flipCard(card, 100);
-    })
-})
-
-
+function initRows() {
+    var cardsFirst = [
+        new Card('People', '<i class="icon-scout-trooper-by-radiusss"></i>', null).setLink(api + 'people/'),
+        new Card('Planets', '<i class="icon-geonosis-by-radiusss"></i>', null).setLink(api + 'planets/'),
+        new Card('Films', '<i class="icon-miscellaneous-by-radiusss"></i>', null).setLink(api + 'films/')
+    ];
+    var cardsSecond = [
+        new Card('Species', '<i class="icon-lightsaber-by-radiusss"></i>', null).setLink(api + 'species/'),
+        new Card('Vehicles', '<i class="icon-speeder-by-radiusss"></i>', null).setLink(api + 'vehicles/'),
+        new Card('Starships', '<i class="icon-tie-fighter-by-radiusss"></i>', null).setLink(api + 'starships/')
+    ];
+    var firstRow = new Row(cardsFirst);
+    var secondRow = new Row(cardsSecond);
+    return [firstRow, secondRow];
+}
+var island = new Island(initRows());
+console.log(island);
+wrapper.appendChild(island.element);
